361cb5c45285ecf5981ae81014ab28e56f3bf23c
hariprasad.allaka@playngo.com <hariprasad.allaka@playngo.com>
2019-10-21 08:31:57 +0200

    fixing null reference errors

--- outputs.tf
+++ outputs.tf
@@ -24,17 +24,17 @@ output "vpc_default_network_acl_id" {
 }
 
 output "vpc_default_security_group_id" {
-  value       = element(concat(aws_vpc.default.*.default_security_group_id,list(""),0)
+  value       = element(concat(aws_vpc.default.*.default_security_group_id,list("")),0)
   description = "The ID of the security group created by default on VPC creation"
 }
 
 output "vpc_default_route_table_id" {
-  value       = element(concat(aws_vpc.default.*.default_route_table_id,list(""),0)
+  value       = element(concat(aws_vpc.default.*.default_route_table_id,list("")),0)
   description = "The ID of the route table created by default on VPC creation"
 }
 
 output "vpc_ipv6_association_id" {
-  value       = element(concat(aws_vpc.default.*.ipv6_association_id,list(""),0)
+  value       = element(concat(aws_vpc.default.*.ipv6_association_id,list("")),0)
   description = "The association ID for the IPv6 CIDR block"
 }
 
